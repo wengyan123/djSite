0b5d0ced4a1140c0769a6cd59fd85f5dd59562f9
wengyan <wengyan123@gmail.com>
2017-03-13 11:53:23 +0800

    admin

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -17,13 +17,12 @@ class Publisher(models.Model):
 
 
 class Author(models.Model):
-    salutation = models.CharField(max_length=10)
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    headshot = models.ImageField(upload_to='author_headshots')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField(blank=True, verbose_name ='e-mail')
 
     def __str__(self):
-        return self.name
+        return u'%s %s' % (self.first_name, self.last_name)
 
 
 class Book(models.Model):
@@ -31,3 +30,6 @@ class Book(models.Model):
     authors = models.ManyToManyField('Author')
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^display_meta/$', views.display_meta, name='display_meta'),
     url(r'^search/$', views.search),
     url(r'^contact/$', views.contact),
+    url(r'^', include('django.contrib.auth.urls')),
 ]
