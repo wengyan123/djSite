16febe924ed75ac862fc721487f1c8364c1e57d6
wengyan <wengyan123@gmail.com>
2017-03-16 14:32:19 +0800

    custom model manager

--- djapps/blog/models.py
+++ djapps/blog/models.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -20,6 +24,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()  # the default manager
+    published = PublishedManager()  # our custom manager
 
     class Meta:
         ordering = ('-publish', )
