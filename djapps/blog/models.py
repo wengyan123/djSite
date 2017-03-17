b470e362ebfaaa34bdafd096562ceb5116d449ff
Weng Yan <wengyan123@gmail.com>
2017-03-18 00:43:00 +0800

    add template

--- djapps/blog/models.py
+++ djapps/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -32,5 +34,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
+
 
 
--- djapps/blog/views.py
+++ djapps/blog/views.py
@@ -4,17 +4,16 @@ from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
+    print posts
     return render(request,
                   'blog/post/list.html',
-                  {'post': posts})
+                  {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                             status='published',
-                             publish_year=year,
-                             publish_month=month,
-                             publish_day=day)
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
