01da35bf0e7c2ec0df14e86a82355e73c0154d16
wengyan <wengyan123@gmail.com>
2017-03-31 12:43:38 +0800

    handle forms in views

--- djapps/djapps/settings.py
+++ djapps/djapps/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
